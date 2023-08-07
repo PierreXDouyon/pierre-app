3010650d4b443b6267d90b00642262e0d1992241
Pierre Douyon <pierredouyon514@gmail.com>
2023-08-06 21:35:29 -0400

    tests defined

--- tests/unit/pierre-app.spec.js
+++ tests/unit/pierre-app.spec.js
@@ -7,3 +7,42 @@
 // MovieDetails.vue - This component will display the details of the recommended movie.
 // pierre-app.vue - The main app component that integrates all the other components.
 // styles.css - A stylesheet file to apply CSS and styles for the components.
+
+
+describe("pierre-app.vue", () => {
+    // RecommendButton Component
+    it("displays a 'Recommend' button", () => {
+      // Test if the "Recommend" button shows up.
+      // Vue.js Code: Use <button> tag to create the button element.
+    });
+  
+    // RecommendButton and RecommendMovie Components
+    it("handles button clicks", () => {
+      // Test if the app responds when the "Recommend" button is clicked.
+      // Vue.js Code: Add the '@click' directive to the button and write a function to handle the click event.
+    });
+  
+    // RecommendButton and RecommendMovie Components
+    it("picks a random movie when button is clicked", () => {
+      // Test if clicking the "Recommend" button gives us a random movie suggestion.
+      // Vue.js Code: Create a function in RecommendMovie to pick a random movie from a list.
+    });
+  
+    // RecommendButton and RecommendMovie Components
+    it("updates movie suggestion with new click", () => {
+      // Test if clicking the "Recommend" button again provides a different movie suggestion.
+      // Vue.js Code: Modify the picking function in RecommendMovie to update with each new click.
+    });
+  
+    // RecommendMovie and MovieDetails Components
+    it("shows movie details after suggestion", () => {
+      // Test if movie details show up after clicking the "Recommend" button.
+      // Vue.js Code: Pass the selected movie's data from RecommendMovie to MovieDetails using props.
+    });
+  
+    // App's Styling
+    it("looks nice", () => {
+      // Test if the app looks visually appealing and well-styled.
+      // Vue.js Code: Apply CSS classes or styles to components to make them visually appealing.
+    });
+  });
