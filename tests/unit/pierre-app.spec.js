dcf019dfd672adafc67f7a99a70cde64b4d90128
Pierre Douyon <pierredouyon514@gmail.com>
2023-08-06 21:08:41 -0400

    brief outline of app & comps in pierre-app.spec.js

---
+++ tests/unit/pierre-app.spec.js
@@ -0,0 +1,9 @@
+// Movie Recommendations App
+// Lets users click a "Recommend" button for random movie suggestions.
+// It uses an external movie database API to fetch movie info.
+
+// RecommendButton.vue - This component will display the "Recommend" button.
+// RecommendMovie.vue - This component will handle movie recommendation logic and API interactions.
+// MovieDetails.vue - This component will display the details of the recommended movie.
+// pierre-app.vue - The main app component that integrates all the other components.
+// styles.css - A stylesheet file to apply CSS and styles for the components.
