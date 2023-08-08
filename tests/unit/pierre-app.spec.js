22565575619363077ffc855b12470a1c3957cdec
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-08 16:49:12 -0400

    updated pierre-app.spec.js for initial display

--- tests/unit/pierre-app.spec.js
+++ tests/unit/pierre-app.spec.js
@@ -1,8 +1,32 @@
-describe("CourseItem.vue", () => {
+// when the user loads the app they should see an example of a movie's details and description,
+//  along with a recommend button
+describe("pierre-app.vue", () => {
+  // RecommendButton Component
+  describe("RecommendButton Component", () => {
+    it("displays a 'Recommend' button", () => {
+      // Test if the "Recommend" button shows up.
+      // Vue.js Code: Use <button> tag to create the button element.
+    });
+  });
 
-// Movie Recommendations App
-// Lets users click a "Recommend" button for random movie suggestions.
-// It uses a local database to fetch movie info.
+  // MovieDetails Component
+  describe("MovieDetails Component", () => {
+    it("shows an example movie's details", () => {
+      // Test if the component displays an example movie's details.
+      // Vue.js Code: Use props to pass example movie data to the 'MovieDetails' component.
+      // Interaction: User sees an example movie's details.
+    });
 
+    it("displays 'View Description' button under movie details", () => {
+      // Test if the component displays the 'View Description' button.
+      // Vue.js Code: Use <button> tag to create the button element.
+      // Interaction: User sees the 'View Description' button under movie details.
+    });
+
+    it("displays movie's description when 'View Description' is clicked", () => {
+      // Test if clicking the "View Description" button displays the movie's description.
+      // Vue.js Code: Use a button with '@click' directive to toggle description visibility.
+      // Interaction: User clicks the button to view the movie's description.
+    });
+  });
 });
-
