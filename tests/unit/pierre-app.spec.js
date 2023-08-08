10358e1cea64d4d15466791527188f2f1911e6eb
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-08 16:28:00 -0400

    put outline of my app in pierre-app.spec.js

--- tests/unit/pierre-app.spec.js
+++ tests/unit/pierre-app.spec.js
@@ -1,69 +1,8 @@
+describe("CourseItem.vue", () => {
+
 // Movie Recommendations App
 // Lets users click a "Recommend" button for random movie suggestions.
 // It uses a local database to fetch movie info.
 
-describe("pierre-app.vue", () => {
-  // RecommendButton Component
-  describe("RecommendButton Component", () => {
-    it("displays a 'Recommend' button", () => {
-      // Test if the "Recommend" button shows up.
-      // Vue.js Code: Use <button> tag to create the button element.
-    });
-
-    it("makes genre selector visible after 'Recommend' button is clicked for the first time", () => {
-      // Test if clicking the "Recommend" button for the first time makes the genre selector visible.
-      // Vue.js Code: Modify the button click function to toggle the genre selector's visibility.
-      // Interaction: The 'RecommendButton' component triggers the visibility of the 'GenreSelector' component.
-    });
-
-    it("makes genre selector visible after 'Recommend' button is clicked again", () => {
-      // Test if clicking the "Recommend" button again makes the genre selector visible.
-      // Vue.js Code: Modify the button click function to toggle the genre selector's visibility.
-      // Interaction: Subsequent button clicks toggle the visibility of the genre selector.
-    });
-  });
-
-  // GenreSelector Component
-  describe("GenreSelector Component", () => {
-    it("displays pick a genre text with a dropdown menu", () => {
-      // Test if the component displays the pick a genre text and a dropdown menu.
-      // Vue.js Code: Use <p> and <select> tags to create the text and dropdown elements.
-      // Interaction: User selects a genre from the dropdown.
-    });
-
-    it("retrieves a random movie based on selected genre", () => {
-      // Test if clicking the "Select Genre" button retrieves a random movie suggestion.
-      // Vue.js Code: Create a function to pick a random movie based on the selected genre.
-      // Interaction: The 'GenreSelector' component retrieves a random movie suggestion from the database.
-    });
-  });
-
-  // MovieDetails Component
-  describe("MovieDetails Component", () => {
-    it("shows movie details after genre selection", () => {
-      // Test if movie details show up after selecting a genre from the GenreSelector.
-      // Vue.js Code: Pass the selected movie's data to the 'MovieDetails' component using props.
-      // Interaction: The 'MovieDetails' component displays movie info received from 'GenreSelector'.
-    });
-
-    it("displays movie's description when 'View Description' is clicked", () => {
-      // Test if clicking the "View Description" button displays the movie's description.
-      // Vue.js Code: Use a button with '@click' directive to toggle description visibility.
-      // Interaction: User clicks the button to view the movie's description.
-    });
-
-    it("picks a random movie when 'Recommend' button is clicked", () => {
-      // Test if clicking the "Recommend" button gives us a random movie suggestion.
-      // Vue.js Code: Create a function to pick a random movie based on the selected genre.
-      // Interaction: The 'MovieDetails' component generates random movie suggestions based on the selected genre.
-    });
-
-    it("updates movie suggestion with new click", () => {
-      // Test if clicking the "Recommend" button again provides a different movie suggestion.
-      // Vue.js Code: Modify the picking function to update with each new click.
-      // Interaction: Subsequent button clicks trigger updated movie suggestions.
-    });
-  });
 });
-
 
