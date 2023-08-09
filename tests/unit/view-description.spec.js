468a496bf429763d44af7291e2be036e4fcec73c
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-09 15:16:22 -0400

    new it statements to view-description.spec.js

--- tests/unit/view-description.spec.js
+++ tests/unit/view-description.spec.js
@@ -1,20 +1,26 @@
 /// ViewDescription Component
 describe("ViewDescription Component", () => {
-it("should hide(with movie description being set to false in the movie array)movie description initially", () => {
-    // Test if the movie description is not visible initially with true and false test.
-    // Vue.js Code: by default the descripyion value is false when clicked it becomes true.
-  });
-
   it("should display 'View Description' button under movie details", () => {
     // Test if the component displays the 'View Description' button under movie details.
     // Vue.js Code: Use <button> tag to create the button element.
     // Interaction: User sees the 'View Description' button under movie details.
   });
-
+    
+it("should hide(with movie description being set to false in the movie array)movie description initially", () => {
+    // Test if the movie description is not visible initially with true and false test.
+    // Vue.js Code: by default the description value is false when clicked it becomes true.
+  });
+  
   it("should show movie description from the array in props.movie-details when 'View Description' is clicked using $emit to toggle description visibility", () => {
     // Test if clicking the "View Description" button emits a custom event to show the movie's description.
     // Vue.js Code: Use a button with '@click' directive to emit a custom event.
     // Interaction: User clicks the button to view the movie's description.
   });
-    
+  
+  it("updates movie suggestion when a genre is clicked again", () => {
+    // Test if clicking a genre again updates the movie suggestion.
+    // Vue.js Code: Simulate clicking a genre, then clicking it again to trigger an updated movie suggestion.
+    // Interaction: Clicking the same genre again should result in a different movie suggestion.
+  });
+
 });
