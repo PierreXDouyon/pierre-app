d1cab8affb729fd81c7064b2d88c83c34c7efac4
Pierre Douyon <pierredouyon514@gmail.com>
2023-08-08 22:50:56 -0400

    updated my it statements

--- tests/unit/pierre-app.spec.js
+++ tests/unit/pierre-app.spec.js
@@ -15,7 +15,7 @@ describe("pierre-app.vue", () => {
       // Vue.js Code: Use the text "Recommend" inside the button.
     });
   });
-});
+
   // MovieDetails Component
   describe("MovieDetails Component", () => {
     it("should display movie details (image, title, genre, release date, IMDB rating) from an array in app.vue", () => {
@@ -36,15 +36,15 @@ describe("pierre-app.vue", () => {
       // Information Source: Example movie data passed as props.
     });
 
+    it("should hide(with movie description being set to false in the movie array)movie description initially", () => {
+      // Test if the movie description is not visible initially with true and false test.
+      // Vue.js Code: by default the descripyion value is false when clicked it becomes true.
+    });
+
     it("should display 'View Description' button under movie details", () => {
       // Test if the component displays the 'View Description' button under movie details.
       // Vue.js Code: Use <button> tag to create the button element.
       // Interaction: User sees the 'View Description' button under movie details.
-    });
-
-    it("should hide movie description initially", () => {
-      // Test if the movie description is not visible initially.
-      // Vue.js Code: Use CSS or data properties to control visibility.
     });
 
     it("should show movie description when 'View Description' is clicked", () => {
@@ -59,23 +59,23 @@ describe("pierre-app.vue", () => {
       // Information Source: Example movie data passed as props.
     });
 
-    it("should display an example movie's genre", () => {
-      // Test if the movie's genre is displayed.
-      // Vue.js Code: Access the example movie's genre from the passed props.
-      // Information Source: Example movie data passed as props.
-    });
+    // it("should display an example movie's genre", () => {
+    //   // Test if the movie's genre is displayed.
+    //   // Vue.js Code: Access the example movie's genre from the passed props.
+    //   // Information Source: Example movie data passed as props.
+    // });
 
-    it("should display an example movie's release date", () => {
-      // Test if the movie's release date is displayed.
-      // Vue.js Code: Access the example movie's release date from the passed props.
-      // Information Source: Example movie data passed as props.
-    });
+    // it("should display an example movie's release date", () => {
+    //   // Test if the movie's release date is displayed.
+    //   // Vue.js Code: Access the example movie's release date from the passed props.
+    //   // Information Source: Example movie data passed as props.
+    // });
 
-    it("should display an example movie's IMDB rating", () => {
-      // Test if the movie's IMDB rating is displayed.
-      // Vue.js Code: Access the example movie's IMDB rating from the passed props.
-      // Information Source: Example movie data passed as props.
-    });
+    // it("should display an example movie's IMDB rating", () => {
+    //   // Test if the movie's IMDB rating is displayed.
+    //   // Vue.js Code: Access the example movie's IMDB rating from the passed props.
+    //   // Information Source: Example movie data passed as props.
+    // });
 });
 
 // RecommendButton Component
@@ -85,13 +85,13 @@ describe("RecommendButton Component", () => {
   //   // Vue.js Code: Use <button> tag to create the button element.
   // });
 
-  it("makes genre selector visible after 'Recommend' button is clicked", () => {
+  it("makes genre selector visible with 'Pick a Genre' text and dropdown menu (Genres = action, romance, comedy, drama) after 'Recommend' button is clicked with a true and false toggle", () => {
     // Test if clicking the "Recommend" button makes the genre selector visible.
     // Vue.js Code: Modify the button click function to toggle genre selector's visibility.
     // Interaction: Clicking the "Recommend" button triggers the visibility of the genre selector.
   });
 
-  it("hides genre selector after a genre is clicked", () => {
+  it("hides genre selector after a genre is clicked with a true and false toggle", () => {
     // Test if genre selector hides after a genre is clicked.
     // Vue.js Code: Modify the genre click function to hide the genre selector.
     // Interaction: After selecting a genre, the genre selector becomes hidden.
@@ -99,20 +99,20 @@ describe("RecommendButton Component", () => {
 });
 
 // GenreSelector Component
-describe("GenreSelector Component", () => {
-  it("displays 'Pick a Genre' (Genres = action, romance, comedy, drama) text and dropdown menu", () => {
-    // Test if the component displays the "Pick a Genre" text and a dropdown menu.
-    // Vue.js Code: Use <p> and <select> tags to create the text and dropdown elements.
-    // Interaction: User sees the "Pick a Genre" text and can select a genre from the dropdown.
-  });
+// describe("GenreSelector Component", () => {
+//   it("displays 'Pick a Genre' text and dropdown menu (Genres = action, romance, comedy, drama) with a true or false toggle", () => {
+//     // Test if the component displays the "Pick a Genre" text and a dropdown menu.
+//     // Vue.js Code: Use <p> and <select> tags to create the text and dropdown elements.
+//     // Interaction: User sees the "Pick a Genre" text and can select a genre from the dropdown.
+//   });
 
-  it("hides genre selector after a genre is clicked", () => {
-    // Test if genre selector hides after a genre is clicked.
-    // Vue.js Code: Modify the genre click function to hide the genre selector.
-    // Interaction: After selecting a genre, the genre selector becomes hidden.
-  });
+  // it("hides genre selector after a genre is clicked", () => {
+  //   // Test if genre selector hides after a genre is clicked.
+  //   // Vue.js Code: Modify the genre click function to hide the genre selector.
+  //   // Interaction: After selecting a genre, the genre selector becomes hidden.
+  // });
 
-  it("should access the movie details array and apply Math.random to generate a random movie suggestion", () => {
+  it("should access the movie details array and apply Math.random to generate a random movie suggestion after a genre has been clicked", async () => {
     // Test if the component accesses the movie details array based on the selected genre.
     // Vue.js Code: Use the selected genre to filter the movie details array.
     // Interaction: User selects a genre, component generates a random movie suggestion.
@@ -139,35 +139,35 @@ describe("MovieDetails Component", () => {
   //   // Interaction: User clicks the button to view the movie's description.
   // });
 
-  it("displays movie details after a genre is clicked", () => {
+  it("displays movie details after a genre is clicked from the genre selector", () => {
     // Test if the component displays movie details after a genre is clicked.
     // Vue.js Code: Use props to pass example movie data to the component.
     // Interaction: User sees movie details after selecting a genre.
   });
 
-  it("should access the movie details array based on the selected genre", () => {
+  it("should access the movie details array based on the selected genre to create a filtered array", () => {
     // Test if the component accesses the movie details array based on the selected genre.
     // Vue.js Code: Use the selected genre to filter the movie details array.
     // Interaction: Component retrieves movie details based on the selected genre.
   });
 
-  it("should use Math.random to select a random movie from the filtered array", () => {
+  it("should use Math.random to select a single random movie from the filtered array", () => {
     // Test if the component uses Math.random to select a random movie suggestion from the filtered array.
     // Vue.js Code: Use Math.random to generate a random index and select a movie.
     // Interaction: Component generates a random movie suggestion from the filtered array.
   });
 
-  it("updates movie suggestion with new click", () => {
+  it("updates movie suggestion with new click of the recommend button", () => {
     // Test if clicking the "Recommend" button again provides a different movie suggestion.
     // Vue.js Code: Modify the picking function to update with each new click.
     // Interaction: Subsequent button clicks trigger updated movie suggestions.
   });
 
-  it("displays an image of the selected movie using JavaScript code", () => {
-    // Test if the component displays an image of the selected movie using JavaScript.
-    // Vue.js Code: Use the selected genre's name and Math.random to display an image.
-    // Interaction: User sees an image of the selected movie in the component.
-  });
+  // it("displays an image of the selected movie using JavaScript code", () => {
+  //   // Test if the component displays an image of the selected movie using JavaScript.
+  //   // Vue.js Code: Use the selected genre's name and Math.random to display an image.
+  //   // Interaction: User sees an image of the selected movie in the component.
+  // });
 });
 
 
@@ -188,7 +188,7 @@ describe("MovieDetails Component", () => {
 |            Genre Selector         |
 +-----------------------------------+
 |                                   |
-|   Pick a Genre:                  |
+|   Pick a Genre:                   |
 |   [ Action ]                      |
 |   [ Romance ]                     |
 |   [ Comedy ]                      |
@@ -197,8 +197,8 @@ describe("MovieDetails Component", () => {
 +-----------------------------------+
 |        Movie Details              |
 +-----------------------------------+
-|                                   |
-|   Title:      Action              |
+|          Image                    |
+|   Title:      Love Movie          |
 |   Genre:      Romance             |
 |   Release:    Jan 1, 2023         |
 |   Rating:     8.5                 |
@@ -212,7 +212,7 @@ describe("MovieDetails Component", () => {
 |   This is a sample movie          |
 |   description. Lorem ipsum dolor  |
 |   sit amet, consectetur           |
-|   adipiscing elit.               |
+|   adipiscing elit.                |
 |                                   |
 +-----------------------------------+
 
