ae4a961679173e5fd9ad055cb36dcd8397a0dd15
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-15 14:35:24 -0400

    fixed error w/ scrolltotop for multiple clicks

--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -4,20 +4,39 @@
 
 <script>
 export default {
-  name: "ScrollToTop",
   data() {
     return {
-      showscrollToTop: false,
+      showButton: false,
     };
   },
   methods: {
     scrollToTop() {
-      const c = document.documentElement.scrollTop || document.body.scrollTop;
-      if (c > 0) {
-        window.requestAnimationFrame(this.scrollToTop);
-        window.scrollTo(0, c - c / 8);
+      const scrollStep = -window.scrollY / (500 / 15);
+      const scrollInterval = setInterval(() => {
+        if (window.scrollY !== 0) {
+          window.scrollBy(0, scrollStep);
+        } else {
+          clearInterval(scrollInterval);
+          this.showButton = false; // Hide the button after reaching top
+          this.scrollToElement("h1"); // Scroll to the element with ID "h1"
+        }
+      }, 15);
+    },
+    handleScroll() {
+      this.showButton = window.scrollY > 100;
+    },
+    scrollToElement(id) {
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
       }
     },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
