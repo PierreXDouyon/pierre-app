9578ef8327b3b52f02ef967e05d72f0f7bb9e530
Pierre Douyon <pierredouyon514@gmail.com>
2023-08-15 21:26:29 -0400

    added working test: genre click, & feat movie x2

--- tests/unit/movie-details.spec.js
+++ tests/unit/movie-details.spec.js
@@ -22,4 +22,60 @@ describe("MovieDetails.vue", () => {
     // Check if movie details are displayed
     expect(wrapper.find(".movie-details").exists()).toBe(true);
   });
+
+  it("renders as a featured movie when isfeatured is true", () => {
+    // Create a sample movie object
+    const movie = {
+      id: 1,
+      title: "Featured Movie",
+      image: "path/to/image.jpg",
+      genre: "Action",
+      releaseDate: "2023-08-01",
+      imdbRating: 8.5,
+      description: "A description for the featured movie.",
+    };
+
+    // Mount the component with isfeatured set to true
+    const wrapper = shallowMount(MovieDetails, {
+      propsData: {
+        movie,
+        isfeatured: true,
+      },
+    });
+
+    // Check if the component is rendered with the featured-details class
+    const featuredDetailsElement = wrapper.find(".featured-details");
+    expect(featuredDetailsElement.exists()).toBe(true);
+
+    // Check if the title is rendered
+    expect(wrapper.find("h2").text()).toBe(movie.title);
+  });
+
+  it("renders as a regular movie when isfeatured is false", () => {
+    // Create a sample movie object
+    const movie = {
+      id: 2,
+      title: "Regular Movie",
+      image: "path/to/image.jpg",
+      genre: "Drama",
+      releaseDate: "2023-08-15",
+      imdbRating: 7.2,
+      description: "A description for the regular movie.",
+    };
+
+    // Mount the component with isfeatured set to false
+    const wrapper = shallowMount(MovieDetails, {
+      propsData: {
+        movie,
+        isfeatured: false,
+      },
+    });
+
+    // Check if the component is rendered with the movie-details class
+    const movieDetailsElement = wrapper.find(".movie-details");
+    expect(movieDetailsElement.exists()).toBe(true);
+
+    // Check if the title is rendered
+    expect(wrapper.find("h2").text()).toBe(movie.title);
+  });
 });
--- tests/unit/movie-list.spec.js
+++ tests/unit/movie-list.spec.js
@@ -80,4 +80,102 @@ describe("MovieList.vue", () => {
     // Check if the featured movie is not included in the filteredMovies array
     expect(filteredMovies).not.toContainEqual(wrapper.vm.featured);
   });
+
+  it("shuffles the movie array when isfeatured is false and selectedGenre changes", async () => {
+    // Create a sample movies array
+    const movies = [
+      {
+        id: 8,
+        title: "John Wick: Chapter 4",
+        image:
+          "https://placehold.co/300x200/ff6700/eee?font=raleway&text=ACTION",
+        genre: "Action",
+        releaseDate: "2023-05-27",
+        imdbRating: 7.8,
+        description:
+          "John Wick is on the run after killing a member of the international assassin’s guild, and with a $14 million price tag on his head, he is the target of all hit men/women.",
+      },
+      {
+        id: 9,
+        title: "The Covenant",
+        image:
+          "https://placehold.co/300x200/ff6700/eee?font=raleway&text=ACTION",
+        genre: "Action",
+        releaseDate: "2023-01-13",
+        imdbRating: 7.5,
+        description:
+          "A group of young witches must protect their secret from an ancient enemy who wants to destroy them.",
+      },
+      {
+        id: 10,
+        title: "Kill Boksoon",
+        image:
+          "https://placehold.co/300x200/ff6700/eee?font=raleway&text=ACTION",
+        genre: "Action",
+        releaseDate: "2023-03-10",
+        imdbRating: 7.2,
+        description:
+          "A female assassin seeks revenge on the gangsters who killed her family and framed her for murder.",
+      },
+      {
+        id: 11,
+        title: "The Super Mario Bros.Movie",
+        image:
+          "https://placehold.co/300x200/ff6700/eee?font=raleway&text=ACTION",
+        genre: "Action",
+        releaseDate: "2023-12-21",
+        imdbRating: 6.8,
+        description:
+          "Mario and Luigi embark on a quest to save Princess Peach from the evil King Bowser.",
+      },
+      {
+        id: 12,
+        title: "Black Panther II",
+        image:
+          "https://placehold.co/300x200/ff6700/eee?font=raleway&text=ACTION",
+        genre: "Action",
+        releaseDate: "2022-11-11",
+        imdbRating: 7.5,
+        description:
+          "The sequel to the 2018 blockbuster, Black Panther, follows the new adventures of King T’Challa and his allies in Wakanda.",
+      },
+      {
+        id: 13,
+        title: "The Matrix Resurrections",
+        image:
+          "https://placehold.co/300x200/ff6700/eee?font=raleway&text=ACTION",
+        genre: "Action",
+        releaseDate: "2021-12-22",
+        imdbRating: 7.4,
+        description:
+          "Neo and Trinity return to the virtual reality of the Matrix, where they face new enemies and old allies.",
+      },
+    ];
+    const initialMovies = [...movies];
+    // Mount the component with the movies array, isfeatured set to false, and a selectedGenre
+    const wrapper = shallowMount(MovieList, {
+      propsData: {
+        movies: initialMovies, // Clone the array to avoid mutations in the test
+        isfeatured: false,
+      },
+    });
+
+    // Simulate changing the selectedGenre
+    await wrapper.setProps({ selectedGenre: "Action" });
+
+    // Get the MovieDetails components from the wrapper
+    const movieDetailsComponents = wrapper.findAllComponents({
+      name: "MovieDetails",
+    });
+
+    // Extract the movie IDs from the MovieDetails components
+    const movieIds = movieDetailsComponents.map(
+      (component) => component.props("movie").id
+    );
+
+    // Check that the movie IDs are not in the original order
+    expect(movieIds).toEqual(
+      expect.arrayContaining(initialMovies.map((movie) => movie.id))
+    );
+  });
 });
